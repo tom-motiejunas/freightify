ad33aa39c84e0c3e0ac1e170cf1cf09d7504bc6e
tom-motiejunas <tom.motiejunas@gmail.com>
2022-09-22 09:33:06 +0000

    first commit

---
+++ stacks/index.ts
@@ -0,0 +1,11 @@
+import { App } from "@serverless-stack/resources";
+
+import { MyStack } from "./MyStack";
+
+export default function main(app: App) {
+  app.setDefaultFunctionProps({
+    runtime: "nodejs16.x",
+    srcPath: "backend",
+  });
+  app.stack(MyStack);
+}
